a8c2c2fccee36c55fd53ee777d32fc21135d05c9
Csanád Bakos <bakoscsanad@gmail.com>
2022-11-26 19:53:06 +0100

    WIP hierarchical CM

--- src/random_graphs/hierarchical_cm.py
+++ src/random_graphs/hierarchical_cm.py
@@ -1,46 +1,51 @@
 from typing import List, Dict, Tuple
 
 import networkx as nx
+import numpy as np
 from matplotlib import pyplot as plt
 import random
 
 
 def hierarchical_configuration_model(deg_seq_in: List[int],
                                      deg_seq_out: List[int],
-                                     communities: Dict[int:List[int]],
+                                     communities: List[int],
                                      seed=0):
     """
 
     :param deg_seq_in: a list of integers, ith item denotes the degree of the ith vertex within its community
     :param deg_seq_out: a list of integers, ith item denotes the degree of the ith vertex matching to other communities
-    :param communities: Dictionary mapping community ids to list of vertices (disjoint communities)
+    :param communities: a list of integers, ith item denotes the community id of the ith vertex
     :param seed:
     :return:
     """
 
     assert len(deg_seq_in) == len(deg_seq_out)
+
+    # Run configuration model for each community, use degree sequence meant for within communities
     community_sub_graphs = {}
-    for k, v in communities:
-        community_sub_graphs[k] = nx.configuration_model(deg_sequence=deg_seq_in[v], seed=seed)
+    for c in range(len(np.unique(communities))):
+        # get vertex ids for current community
+        vertex_ids_for_c = np.where(communities == c)
+        community_sub_graphs[c] = nx.configuration_model(deg_sequence=deg_seq_in[vertex_ids_for_c], seed=seed)
 
     random.seed(seed)
-    # algo 3: choose a remaining half edge uniformly, then choose another half edge uniformly, mathc them, update
+    # algo 3: choose a remaining half edge uniformly, then choose another half edge uniformly, match them, update
     # remaining half edges
     # addition for hierarchical CM: choose pair h.e. uniformly from any other community
 
-    # create ids for half edges:
+    # create list for half edges:
     # for each h.e. we need to know:
-    # - which vertex it belongs
-    # - which community it belongs
+    # - its vertex
+    # - its community
     # map id to a tuple of vertex id and community id
-    half_edges: Dict[int: Tuple[int, int]] = {}
+    # should be easy to remove h.e.
+    half_edges: List[Tuple[int, int]] = []
     id = 0
     for i in range(len(deg_seq_out)):
         for j in range(deg_seq_out[i]):
             half_edges[id] = (i, communities)
 
     random.randint()
-
 
 
 if "__main__" == __name__:
