da5c1fdc5fb97f1d27e8a4e75986a5ead07455b1
Csanád Bakos <bakoscsanad@gmail.com>
2022-11-26 20:43:37 +0100

    join communities in one graph

--- src/random_graphs/hierarchical_cm.py
+++ src/random_graphs/hierarchical_cm.py
@@ -6,9 +6,9 @@ from matplotlib import pyplot as plt
 import random
 
 
-def hierarchical_configuration_model(deg_seq_in: List[int],
-                                     deg_seq_out: List[int],
-                                     communities: List[int],
+def hierarchical_configuration_model(deg_seq_in: np.array,
+                                     deg_seq_out: np.array,
+                                     communities: np.array,
                                      seed=0):
     """
 
@@ -20,18 +20,22 @@ def hierarchical_configuration_model(deg_seq_in: List[int],
     """
 
     assert len(deg_seq_in) == len(deg_seq_out)
+    full_graph = nx.Graph()
 
     # Run configuration model for each community, use degree sequence meant for within communities
     community_sub_graphs = {}
-    for c in range(len(np.unique(communities))):
+    for c in np.unique(communities):
         # get vertex ids for current community
-        vertex_ids_for_c = np.where(communities == c)
-        community_sub_graphs[c] = nx.configuration_model(deg_sequence=deg_seq_in[vertex_ids_for_c], seed=seed)
+        vertex_ids_for_c = np.where(communities == c)[0]  # index 0 since we have only one dimension
+        # call nx.Graph to get a simple graph -> erased CM model
+        community_sub_graph = nx.Graph(nx.configuration_model(deg_sequence=deg_seq_in[vertex_ids_for_c], seed=seed))
+        community_sub_graphs[c] = community_sub_graph
+        full_graph = nx.disjoint_union(full_graph, community_sub_graph)
 
     random.seed(seed)
     # algo 3: choose a remaining half edge uniformly, then choose another half edge uniformly, match them, update
     # remaining half edges
-    # addition for hierarchical CM: choose pair h.e. uniformly from any other community
+    # addition for hierarchical CM: choose 2nd h.e. uniformly from any other community
 
     # create list for half edges:
     # for each h.e. we need to know:
@@ -39,21 +43,28 @@ def hierarchical_configuration_model(deg_seq_in: List[int],
     # - its community
     # map id to a tuple of vertex id and community id
     # should be easy to remove h.e.
-    half_edges: List[Tuple[int, int]] = []
-    id = 0
-    for i in range(len(deg_seq_out)):
-        for j in range(deg_seq_out[i]):
-            half_edges[id] = (i, communities)
+    num_half_edges = np.sum(deg_seq_out)
+    half_edges = np.zeros((num_half_edges, 2))
+    half_edge_index = 0
+    for v_index in range(len(deg_seq_out)):
+        half_edges[half_edge_index][0] = v_index
+        half_edges[half_edge_index][1] = communities[v_index]
 
-    random.randint()
+    # pick first half edge uniformly from all h.e.-s
+    first = random.randint(0, len(half_edges))
+    first_community = half_edges[first, 1]
+    half_edges_from_other_communities = np.where(half_edges[:] != first_community)
+    second = random.randint(0, len(half_edges_from_other_communities))
+
+    return full_graph
 
 
 if "__main__" == __name__:
     seed = 0
-    deg_seq_in = [5, 3, 3, 3]
-    deg_seq_out = []
-    community = []
-    g = hierarchical_configuration_model(deg_seq_in=deg_seq_in, deg_seq_out=deg_seq_out, communities=community)
+    deg_seq_in = np.array([5, 3, 3, 3])
+    deg_seq_out = np.array([5, 3, 3, 3])
+    communities = np.array([0, 0, 1, 1])
+    g = hierarchical_configuration_model(deg_seq_in=deg_seq_in, deg_seq_out=deg_seq_out, communities=communities)
     pos = nx.spring_layout(g, seed=seed)  # Seed layout for reproducibility
     nx.draw(g, pos=pos)
     plt.show()
